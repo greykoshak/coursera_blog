6116c82e113fa13414f93ea9f1ca287bfe6b79e1
gk <greykoshak@gmail.com>
2019-03-12 21:14:38 +0200

    First

--- mycore/views.py
+++ mycore/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('ok')
+    return render(request, "mycore/index.html")
